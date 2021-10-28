8f4fdca2d163f5f17df22599c8c9439f85137afa
Kartik Prajapat <kr.pjpt@gmail.com>
2021-10-28 21:36:50 +0530

    a

--- bin/configuredb.sh
+++ bin/configuredb.sh
@@ -7,6 +7,6 @@ echo "Configuring database: $database"
 dropdb -U root bank_branch 
 createdb -U root bank_branch 
 
-psql -U root bank_branch < ./sql/branches.sql 
+psql -U root bank_branch < ./bin/sql/branches.sql 
 
 echo "$database configured"
