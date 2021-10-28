1874a85eefa8c0ae57052ed14441521c21327dbe
Kartik Prajapat <kr.pjpt@gmail.com>
2021-10-28 22:08:49 +0530

    b

--- app.js
+++ app.js
@@ -8,16 +8,18 @@ const IP = process.env.IP || "192.168.178.1";
 app.use(express.json());
 
 app.get("/",(request,response) => {
-    pool.query('SELECT * FROM branches',(err,res)=> {
-        if(err) {
-            return res.json({error:err.message});
-        }
-        res.json({
-            success: true,
-            res
-        })
-    });
-})
+
+    // pool.query('SELECT * FROM branches',(err,res)=> {
+    //     if(err) {
+    //         return res.json({error:err.message});
+    //     }
+    //     res.json({
+    //         success: true,
+    //         res
+    //     })
+    // });
+    res.send("hello");
+});
 // app.get("/api/branches/autocomplete?", (request,response) => {
 
 // })
